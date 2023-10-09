87411efba40843ecbc45af9a812748acb01e3198
YuP2905 <y290575685@gmail.com>
2023-10-09 12:41:48 +0900

    None

--- setup.py
+++ setup.py
@@ -11,5 +11,6 @@ setuptools.setup(
     description='Just to Practice Packaging Python Packages',
     long_description=long_description,
     license ='MIT',
+    url='https://github.com/YuP2905/MyModel_Test.git',
     author_email='y290575685@gmail.com',
     packages=setuptools.find_packages(exclude=['Tests']))
